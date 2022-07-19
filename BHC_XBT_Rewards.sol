9d318a57902c578229f1f4336782415d5ceb0f4e
Bitcoin HODL Club <109595437+BitcoinHC@users.noreply.github.com>
2022-07-19 13:32:09 +0100

    Update BHC_XBT_Rewards.sol

--- BHC_XBT_Rewards.sol
+++ BHC_XBT_Rewards.sol
@@ -14,6 +14,8 @@ flattened 19/07/22
 
 */
 
+// File @openzeppelin/contracts/token/ERC20/IERC20.sol@v4.7.0
+
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
 
@@ -96,7 +98,6 @@ interface IERC20 {
         uint256 amount
     ) external returns (bool);
 }
-
 
 /**
  * @dev Contract module that helps prevent reentrant calls to a function.
@@ -254,246 +255,9 @@ abstract contract Ownable is Context {
     }
 }
 
-contract BHC_Presale is Ownable, ReentrancyGuard {
-    bool public active = false;
 
-    address constant private BUSD = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
-    address constant public BHC = 0x5AC9E12edA36ef773A98a4102DD95c43a1F7e500;
+// File contracts/PCS/Router.sol
 
-    uint256 private constant MAX_UINT = type(uint256).max;
-    uint256 BIG_NUM = 10 ** 18;
-
-    receive() external payable {}
-    fallback() external payable {}
-
-    constructor() {
-    }
-
-    function EnterPresale(uint256 amount) public {
-        require(active, "The Presale has not yet started");
-        require((amount >= 10 ether), "Minimum amount is 10 BUSD");
-        uint256 bal = IERC20(BHC).balanceOf(address(this));
-        if(bal >= amount){
-            IERC20(BUSD).transferFrom(msg.sender, address(this), amount);
-            uint256 p = amount * 20;
-            IERC20(BHC).transfer(msg.sender, p);
-        }else{
-            revert("You must purchase a smaller amount");
-        }
-
-    }
-
-    function ActivatePresale() external onlyOwner {
-        active = true;
-    }
-
-    function DeactivatePresale() external onlyOwner {
-        active = false;
-    }
-
-    function WithdrawRemainingBHC() external onlyOwner {
-        uint256 bal = IERC20(BHC).balanceOf(address(this));
-        IERC20(BHC).transfer(owner(), bal);
-    }
-
-    function WithdrawBUSD() external onlyOwner {
-        uint256 bal = IERC20(BUSD).balanceOf(address(this));
-        IERC20(BUSD).transfer(owner(), bal);
-    }
-}
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP.
- */
-interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-}
-abstract contract ReentrancyGuard {
-    // Booleans are more expensive than uint256 or any type that takes up a full
-    // word because each write operation emits an extra SLOAD to first read the
-    // slot's contents, replace the bits taken up by the boolean, and then write
-    // back. This is the compiler's defense against contract upgrades and
-    // pointer aliasing, and it cannot be disabled.
-
-    // The values being non-zero value makes deployment a bit more expensive,
-    // but in exchange the refund on every call to nonReentrant will be lower in
-    // amount. Since refunds are capped to a percentage of the total
-    // transaction's gas, it is best to keep them low in cases like this one, to
-    // increase the likelihood of the full refund coming into effect.
-    uint256 private constant _NOT_ENTERED = 1;
-    uint256 private constant _ENTERED = 2;
-
-    uint256 private _status;
-
-    constructor() {
-        _status = _NOT_ENTERED;
-    }
-
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * Calling a `nonReentrant` function from another `nonReentrant`
-     * function is not supported. It is possible to prevent this from happening
-     * by making the `nonReentrant` function external, and making it call a
-     * `private` function that does the actual work.
-     */
-    modifier nonReentrant() {
-        // On the first call to nonReentrant, _notEntered will be true
-        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
-
-        // Any calls to nonReentrant after this point will fail
-        _status = _ENTERED;
-
-        _;
-
-        // By storing the original value once again, a refund is triggered (see
-        // https://eips.ethereum.org/EIPS/eip-2200)
-        _status = _NOT_ENTERED;
-    }
-}
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
-abstract contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor() {
-        _transferOwnership(_msgSender());
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Internal function without access restriction.
-     */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
-}
 interface IPCSRouter {
 
 
@@ -510,6 +274,10 @@ interface IPCSRouter {
     function WETH() external pure returns (address);
     
 }
+
+
+// File contracts/SolStake/sTOKEN.sol
+
 /*
   _________      .__    _________ __          __           
  /   _____/ ____ |  |  /   _____//  |______  |  | __ ____  
@@ -694,6 +462,9 @@ contract SolStakeToken is ReentrancyGuard, Ownable {
     }
 
 }
+
+
+// File contracts/BHC_XBT_Rewards.sol
 contract BHC_XBT_Rewards is ReentrancyGuard, Ownable, SolStakeToken {
 
     string public name = "BitcoinHODLClub XBT Rewards";
@@ -766,3 +537,4 @@ contract BHC_XBT_Rewards is ReentrancyGuard, Ownable, SolStakeToken {
     }
 
 }
+
